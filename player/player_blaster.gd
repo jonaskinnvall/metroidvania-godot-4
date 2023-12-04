344075b458691711b83dd955a38ff7096ae8a8b0
Jonas Kinnvall <jonas.kinnvall@algodx.com>
2023-12-04 21:56:13 +0100

    feat: add player blaster that is rotated towards mouse position

---
+++ player/player_blaster.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+@onready var blaster_sprite: Sprite2D = $BlasterSprite
+@onready var muzzle: Marker2D = $BlasterSprite/Muzzle
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	blaster_sprite.rotation = get_local_mouse_position().angle()
