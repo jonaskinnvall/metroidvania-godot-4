55311aaf1a25c9b030c84f2ca0e04bdb5aadad0d
Jonas Kinnvall <jonas.kinnvall@algodx.com>
2023-12-09 11:31:17 +0100

    feat: add projectile scene with script for projectiles to inherit from

---
+++ projectile.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+@export var  speed: int = 250
+
+var velocity = Vector2.ZERO
+
+
+func _process(delta: float) -> void:
+	position += velocity * delta
+
+
+func update_rotation() -> void:
+	velocity.x = speed
+	velocity = velocity.rotated(rotation)
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
