4ac1721237cfd12fa25ccef5120e03ab58488e29
Jonas Kinnvall <jonas.kinnvall@algodx.com>
2023-12-15 06:53:46 +0100

    feat: instantiate explosion effect when projectile enters area or body

--- projectile.gd
+++ projectile.gd
@@ -1,8 +1,9 @@
 class_name Projectile
 extends Node2D
 
-@export var  speed: int = 250
+const ExplosionEffectScene: PackedScene = preload('res://effects/explosion_effect.tscn')
 
+@export var  speed: int = 250
 var velocity: Vector2 = Vector2.ZERO
 
 
@@ -20,8 +21,10 @@ func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 
 
 func _on_hitbox_area_entered(_area: Area2D) -> void:
+	Utils.instanstiate_to_world(ExplosionEffectScene, global_position)
 	queue_free()
 
 
 func _on_hitbox_body_entered(_body: Node2D) -> void:
+	Utils.instanstiate_to_world(ExplosionEffectScene, global_position)
 	queue_free()
