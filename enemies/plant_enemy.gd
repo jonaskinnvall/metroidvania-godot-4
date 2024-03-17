c029797ee9c3dc9136d038a1b090e71e86935c60
Jonas Kinnvall <jonas.kinnvall@algodx.com>
2024-03-17 08:38:29 +0100

    fix: plant enemy bullet direction

--- enemies/plant_enemy.gd
+++ enemies/plant_enemy.gd
@@ -1,9 +1,24 @@
 extends Node2D
 
+const EnemyBulletScene: PackedScene = preload('res://enemies/enemy_bullet.tscn')
+
+@export var bullet_speed: int = 30
+@export var spread: int = 30
 @onready var base_stats: BaseStats = $BaseStats
+@onready var bullet_spawn_point: Marker2D = $BulletSpawnPoint
+@onready var fire_direction: Marker2D = $FireDirection
 
 
-func _on_hurtbox_hurt(hitbox: Area2D, damage: int) -> void:
+func fire_bullet() -> void:
+	var bullet: Projectile = Utils.instanstiate_to_world(EnemyBulletScene, bullet_spawn_point.global_position)
+	var direction: float = global_position.angle_to_point(fire_direction.global_position)
+	bullet.rotation = direction
+	bullet.rotate(randf_range(-deg_to_rad(spread/2),deg_to_rad(spread/2))) 
+	bullet.speed = bullet_speed
+	bullet.update_rotation()
+
+
+func _on_hurtbox_hurt(_hitbox: Area2D, damage: int) -> void:
 	base_stats.health -= damage
 
 
