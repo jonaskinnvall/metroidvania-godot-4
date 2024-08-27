f382b7550ee1e51b1a6c3fb0e207d42cf7b56441
Jonas Kinnvall <jonas.kinnvall@algodx.com>
2024-08-27 07:27:00 +0200

    feat: update amount of missiles + missiles ui

--- player/player_stats.gd
+++ player/player_stats.gd
@@ -1,12 +1,14 @@
 extends BaseStats
 
-@export var max_missiles: int = 3: set = set_max_missiles
-@onready var missiles: int = 3: set = set_missiles
+@export var max_missiles: int = 0: set = set_max_missiles
+@onready var missiles: int = max_missiles: set = set_missiles
 
+signal max_missiles_changed
 signal missiles_count_changed
 
 func set_max_missiles(value: int) -> void:
 	max_missiles = value
+	max_missiles_changed.emit()
 
 func set_missiles(value: int) -> void:
 	missiles = clampi(value, 0, max_missiles)
--- ui/missile_ui.gd
+++ ui/missile_ui.gd
@@ -3,8 +3,13 @@ extends HBoxContainer
 @onready var label: Label = $Label
 
 func _ready() -> void:
+	_max_missiles_changed()
 	update_missiles_label()
 	PlayerStats.missiles_count_changed.connect(update_missiles_label)
+	PlayerStats.max_missiles_changed.connect(_max_missiles_changed)
 
 func update_missiles_label() -> void:
 	label.text = str(PlayerStats.missiles)
+
+func _max_missiles_changed() -> void:
+	visible = PlayerStats.max_missiles > 0
