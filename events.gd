55faf073357c80866e67db9ce2b757bbdc912497
Jonas Kinnvall <jonas.kinnvall@algodx.com>
2024-08-30 07:42:29 +0200

    feat: add camera limits

--- events.gd
+++ events.gd
@@ -2,3 +2,4 @@ extends Node
 
 signal add_screenshake(amount: float, duration: float)
 signal door_entered(door: Door)
+signal camera_limits_changed(left: float, right: float, top: float, bottom: float)
--- world/camera_2d.gd
+++ world/camera_2d.gd
@@ -6,6 +6,7 @@ var shake: float = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Events.add_screenshake.connect(set_screenshake)
+	Events.camera_limits_changed.connect(set_camera_limits)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,6 +18,13 @@ func _process(_delta: float) -> void:
 func set_screenshake(amount: float,duration: float) -> void:
 	shake = amount
 	timer.start(duration)
+
+
+func set_camera_limits(left: float, right: float, top: float, bottom: float) -> void:
+	limit_left = int(left)
+	limit_right = int(right)
+	limit_top = int(top)
+	limit_bottom = int(bottom)
 
 
 func _on_timer_timeout() -> void:
