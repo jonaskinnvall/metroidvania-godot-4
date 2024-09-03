c8b2d6b416007619f263facc434a6d9de9c3c1c7
Jonas Kinnvall <jonas.kinnvall@algodx.com>
2024-09-03 06:28:12 +0200

    feat(enemies): change boss to node2d, add hitbox + hurtbox

--- enemies/boss_enemy.gd
+++ enemies/boss_enemy.gd
@@ -1,5 +1,25 @@
-extends CharacterBody2D
+extends Node2D
+
+var state: Callable = _rush_state
+@onready var boss_stats: BaseStats = $BossStats
 
 
-func _physics_process(delta: float) -> void:
-	move_and_slide()
+func _process(_delta: float) -> void:
+	pass
+
+
+func _rush_state(_delta: float) -> void:
+	pass
+
+
+
+func _fire_state(_delta: float) -> void:
+	pass
+
+
+func _on_hurtbox_hurt(_hitbox: Area2D, damage: int) -> void:
+	boss_stats.health -= damage
+
+
+func _on_boss_stats_no_health() -> void:
+	queue_free()
