514f2f50f14d18aed68bd635abee6962cbe855b4
Jonas Kinnvall <jonas.kinnvall@algodx.com>
2024-09-04 18:45:12 +0200

    feat(enemies/boss): add rush state + increase max_health

--- enemies/boss_enemy.gd
+++ enemies/boss_enemy.gd
@@ -1,16 +1,29 @@
 extends Node2D
 
+@export var acceleration: int = 200
+@export var max_speed: int = 800
+
 var state: Callable = _rush_state
+var velocity: Vector2 = Vector2.ZERO
 @onready var boss_stats: BaseStats = $BossStats
 
 
-func _process(_delta: float) -> void:
-	pass
+func _process(delta: float) -> void:
+	state.call(delta)
 
 
-func _rush_state(_delta: float) -> void:
-	pass
+func _rush_state(delta: float) -> void:
+	var player : Player = MainInstances.player
+	if not player is Player: return 
 
+	var direction: Vector2 = global_position.direction_to(player.global_position)
+	velocity = velocity.move_toward(direction * max_speed, acceleration * delta)
+	
+	move(delta)
+
+
+func move(delta: float) -> void:
+	translate(velocity * delta)
 
 
 func _fire_state(_delta: float) -> void:
