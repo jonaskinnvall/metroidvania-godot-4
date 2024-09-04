dec3a94cb1882fb6a4d29d8d53f9e0995444141f
Jonas Kinnvall <jonas.kinnvall@algodx.com>
2024-09-04 19:34:12 +0200

    feat: add fire state where boss fires stingers

--- enemies/boss_enemy.gd
+++ enemies/boss_enemy.gd
@@ -1,11 +1,16 @@
 extends Node2D
 
+const StingerScene: PackedScene = preload('res://enemies/stinger.tscn')
+
 @export var acceleration: int = 200
 @export var max_speed: int = 800
 
-var state: Callable = _rush_state
+var state: Callable = _fire_state
 var velocity: Vector2 = Vector2.ZERO
 @onready var boss_stats: BaseStats = $BossStats
+@onready var fire_timer: Timer = $FireTimer
+@onready var stinger_pivot: Marker2D = $StingerPivot
+@onready var muzzle: Marker2D = $StingerPivot/Muzzle
 
 
 func _process(delta: float) -> void:
@@ -22,12 +27,17 @@ func _rush_state(delta: float) -> void:
 	move(delta)
 
 
+func _fire_state(_delta: float) -> void:
+	if fire_timer.time_left == 0:
+		stinger_pivot.rotation_degrees += 17
+		fire_timer.start()
+		var stinger: Node2D = Utils.instantiate_to_level(StingerScene, muzzle.global_position)
+		stinger.rotation = stinger_pivot.rotation
+		stinger.update_velocity_based_on_rotation()
+
+
 func move(delta: float) -> void:
 	translate(velocity * delta)
-
-
-func _fire_state(_delta: float) -> void:
-	pass
 
 
 func _on_hurtbox_hurt(_hitbox: Area2D, damage: int) -> void:
