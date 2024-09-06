6fb998492cf2a87b8710d28d5a941b88844362a0
Jonas Kinnvall <jonas.kinnvall@algodx.com>
2024-09-06 08:07:48 +0200

    fix: disable trigger after player enters

--- world/level_two.gd
+++ world/level_two.gd
@@ -1,7 +1,9 @@
 extends Node2D
 
 @onready var bricks: Node2D = $Bricks
+@onready var bricks_trigger: Trigger = $BricksTrigger
 
 func _on_bricks_trigger__entered() -> void:
 	if not WorldStash.retrieve('first_boss', 'freed'):
 		bricks.show()
+		bricks_trigger.is_active = false
--- world/trigger.gd
+++ world/trigger.gd
@@ -1,8 +1,11 @@
+class_name Trigger
 extends Area2D
 
 
+var is_active: bool = true
 signal _entered
 
 
 func _on_body_entered(_body: Node2D) -> void:
-	_entered.emit()
+	if is_active:
+		_entered.emit()
