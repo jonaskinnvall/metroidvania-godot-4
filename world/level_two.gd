ed64bfb1757aa69fc657158b969b10eaedad4911
Jonas Kinnvall <jonas.kinnvall@algodx.com>
2024-09-06 07:58:49 +0200

    fix(level-two): only trigger bricks if boss is not defeated

--- world/level_two.gd
+++ world/level_two.gd
@@ -3,4 +3,5 @@ extends Node2D
 @onready var bricks: Node2D = $Bricks
 
 func _on_bricks_trigger__entered() -> void:
-	bricks.show()
+	if not WorldStash.retrieve('first_boss', 'freed'):
+		bricks.show()
