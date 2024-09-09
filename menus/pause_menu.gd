9ce9d5edafdb3bd1a76ba61b8c5ef264dc24258d
Jonas Kinnvall <jonas.kinnvall@algodx.com>
2024-09-09 06:54:09 +0200

    feat: add main menu option to pause menu

--- menus/pause_menu.gd
+++ menus/pause_menu.gd
@@ -21,6 +21,13 @@ func _on_resume_button_pressed() -> void:
 	paused = false
 
 
+func _on_main_menu_button_pressed() -> void:
+	Sound.play(Sound.click, 1.0, -10.0)
+	WorldStash.data = {}
+	get_tree().paused = false
+	get_tree().change_scene_to_file('res://menus/start_menu.tscn')
+
+
 func _on_quit_button_pressed() -> void:
 	Sound.play(Sound.click, 1.0, -10.0)
 	get_tree().quit()
