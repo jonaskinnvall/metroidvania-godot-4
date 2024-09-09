6d66a28538d6216844410cc8329fd13c10577c25
Jonas Kinnvall <jonas.kinnvall@algodx.com>
2024-09-09 06:20:07 +0200

    feat: add animation to save station to indicate saving

--- world/save_station.gd
+++ world/save_station.gd
@@ -1,6 +1,8 @@
 extends StaticBody2D
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
 
 func _on_area_2d_body_entered(_body: Node2D) -> void:
-	print('save')
+	animation_player.play('save')
 	SaveManager.save_to_file()
