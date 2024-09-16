8a75dfef96f17a8f0459050d6dbde6993121df7a
Jonas Kinnvall <jonas.kinnvall@algodx.com>
2024-09-16 08:17:37 +0200

    feat: add player aim with gamepad right stick
    - Does need fixing to feel good

--- player/player_blaster.gd
+++ player/player_blaster.gd
@@ -3,11 +3,16 @@ extends Node2D
 const BulletScene: PackedScene = preload('res://player/bullet.tscn')
 const MissileScene: PackedScene = preload('res://player/missile.tscn')
 
+@export var mouse_speed: int = 15
+
 @onready var blaster_sprite: Sprite2D = $BlasterSprite
 @onready var muzzle: Marker2D = $BlasterSprite/Muzzle
 
 
 func _process(_delta: float) -> void:
+	var mouse_pos: Vector2 = get_viewport().get_mouse_position()
+	get_viewport().warp_mouse(mouse_pos + Input.get_vector("aim_left", "aim_right", "aim_up", "aim_down") * mouse_speed)
+	
 	blaster_sprite.rotation = get_local_mouse_position().angle()
 
 
